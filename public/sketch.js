6b2da9a11cd4134ee79f6d3848049ceaea163be2
jsondoo <jsondoo@users.noreply.github.com>
2017-12-03 22:23:39 -0800

    Add files

---
+++ public/sketch.js
@@ -0,0 +1,84 @@
+var socket;
+var r;
+var g;
+var b;
+var slider;
+var sizecounter;
+
+// runs once at the beginning
+function setup() {
+  socket = io.connect('localhost:3000');
+
+  // these need a call back function
+  socket.on('syncDrawing', newDrawing);
+  socket.on('currentState', currentState);
+  socket.on('usersOnline', usersOnline);
+  socket.on('clear canvas', function() {
+    clear();
+  })
+
+  // assign a random color to the client
+  r = random(100,255);
+  g = random(100,255);
+  b = random(100,255);
+
+  ////////////
+  slider = createSlider(1,8, 5);
+  slider.parent('slider');
+  sizecounter = select(".sizecounter");
+  sizecounter.html(slider.value());
+  var canvas = createCanvas(750,500);
+  canvas.class('canvas');
+  canvas.parent('canvas-holder');
+  background('white');
+  ////////////
+
+  strokeWeight(5); // pen size
+}
+
+// called when the mouse is dragged
+function mouseDragged() {
+  sizecounter = select(".sizecounter");
+  sizecounter.html(slider.value());
+  strokeWeight(slider.value());
+
+  stroke(r,g,b);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+
+  // emit info about RGB
+  var data = {
+    "red": r,
+    "green": g,
+    "blue": b,
+    "x": mouseX,
+    "y": mouseY,
+    "px": pmouseX,
+    "py": pmouseY,
+    "weight": slider.value()
+  }
+  socket.emit('mouseEvent', data);
+}
+
+function currentState(data) {
+  for (var i = 0; i < data.length; i++) {
+    strokeWeight(data[i].weight);
+    stroke(data[i].red, data[i].green, data[i].blue);
+    line(data[i].x, data[i].y, data[i].px, data[i].py);
+  }
+}
+
+function newDrawing(data) {
+  strokeWeight(data.weight);
+  stroke(data.red, data.green, data.blue);
+  line(data.x, data.y, data.px, data.py);
+}
+
+function usersOnline(x) {
+  $('#num_users').text(x.toString()); // .val doesnt work on span
+}
+
+// clears canvas, also empties current state in server
+function clearCanvas(){
+  clear();
+  socket.emit('clear canvas');
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+// Start Project:
+// Run two commands in different terminal windows:
+// In project directory:
+// npm run start
+// In directory with ngrok executable:
+// ./ngrok http 3000 -> Opens connection with localhost:3000
+
+var express = require('express')
+var app = express();
+var server = app.listen(3000);
+
+// tell the app to use the public folder files
+app.use(express.static('public'));
+var socket = require('socket.io');
+var io = socket(server); // tell socket to connect to the server we created
+io.sockets.on('connection', newConnection); // catch a connection, and call newConneciton
+
+var cs = [];
+var usersOnline = 0;
+
+function newConnection(socket) {
+    // console.log(socket.id);
+    usersOnline++;
+
+    console.log(usersOnline)
+
+    io.emit('usersOnline', usersOnline);
+    io.emit('currentState', cs);
+
+    socket.on('clear canvas', function() {
+        cs = [];
+        // clear the canvas for rest of users
+        socket.broadcast.emit('clear canvas');
+    })
+    socket.on('mouseEvent', function(data){
+        cs.push(data);
+        socket.broadcast.emit('syncDrawing', data);
+    });
+    socket.on('disconnect', function() { 
+        usersOnline--;
+        console.log(usersOnline)
+        io.emit('usersOnline', usersOnline);
+    })    
+}
